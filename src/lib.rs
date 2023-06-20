f56820d8b7223f2e41c3f04ef51f6f304a55469e
John Lyon-Smith <john@lyon-smith.org>
2023-06-19 21:10:29 -0700

    Add basic SVG generation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod log_macros;
 use clap::Parser;
 use core::fmt::Arguments;
 use easy_error::{self, ResultExt};
+use hypermelon::{build, prelude::*};
+use serde::Deserialize;
 use std::{
     error::Error,
     fs::File,
@@ -61,6 +63,37 @@ impl Cli {
     }
 }
 
+#[derive(Deserialize, Debug, Clone)]
+pub struct ChartData {
+    pub title: String,
+    pub units: String,
+    pub data: Vec<ItemData>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ItemData {
+    pub key: String,
+    pub value: f64,
+}
+
+#[derive(Debug)]
+struct Gutter {
+    left: f64,
+    top: f64,
+    right: f64,
+    bottom: f64,
+}
+
+#[derive(Debug)]
+struct RenderData {
+    title: String,
+    gutter: Gutter,
+    pie_diameter: f64,
+    styles: Vec<String>,
+    total: f64,
+    tuples: Vec<(String, f64)>,
+}
+
 impl<'a> PieChartTool<'a> {
     pub fn new(log: &'a dyn PieChartLog) -> PieChartTool {
         PieChartTool { log }
@@ -78,13 +111,93 @@ impl<'a> PieChartTool<'a> {
             }
         };
 
-        let mut content = String::new();
+        let chart_data = Self::read_chart_file(cli.get_input()?)?;
+        let render_data = self.process_chart_data(&chart_data)?;
+        let output = self.render_chart(&render_data)?;
 
-        cli.get_input()?.read_to_string(&mut content)?;
-
-        write!(cli.get_output()?, "{}", content)?;
+        Self::write_svg_file(cli.get_output()?, &output)?;
 
         Ok(())
+    }
+
+    fn read_chart_file(mut reader: Box<dyn Read>) -> Result<ChartData, Box<dyn Error>> {
+        let mut content = String::new();
+
+        reader.read_to_string(&mut content)?;
+
+        let chart_data: ChartData = json5::from_str(&content)?;
+
+        Ok(chart_data)
+    }
+
+    fn write_svg_file(mut writer: Box<dyn Write>, output: &str) -> Result<(), Box<dyn Error>> {
+        write!(writer, "{}", output)?;
+
+        Ok(())
+    }
+
+    fn process_chart_data(self: &Self, cd: &ChartData) -> Result<RenderData, Box<dyn Error>> {
+        let mut tuples = vec![];
+        let mut total: f64 = 0.0;
+
+        for item_data in cd.data.iter() {
+            let value = item_data.value;
+
+            total += value;
+
+            tuples.push((item_data.key.to_string(), item_data.value));
+        }
+
+        let pie_diameter = 200.0;
+        let gutter = Gutter {
+            top: 40.0,
+            bottom: 80.0,
+            left: 80.0,
+            right: 80.0,
+        };
+
+        Ok(RenderData {
+            title: cd.title.to_string(),
+            gutter,
+            pie_diameter,
+            styles: vec![
+                ".line{fill:none;stroke:rgb(0,0,200);stroke-width:2;}".to_string(),
+                ".labels{fill:rgb(0,0,0);font-size:10;font-family:Arial}".to_string(),
+                ".title{font-family:Arial;font-size:12;text-anchor:middle;}".to_string(),
+            ],
+            total,
+            tuples,
+        })
+    }
+
+    fn render_chart(self: &Self, rd: &RenderData) -> Result<String, Box<dyn Error>> {
+        let width = rd.gutter.left + ((rd.tuples.len() as f64) * rd.pie_diameter) + rd.gutter.right;
+        let height = rd.gutter.top + rd.gutter.bottom + rd.pie_diameter;
+        let style =
+            build::elem("style").append(build::from_iter(rd.styles.iter().map(|s| s.as_str())));
+
+        let svg = build::elem("svg").with(attrs!(
+            ("xmlns", "http://www.w3.org/2000/svg"),
+            ("width", width),
+            ("height", height),
+            ("viewBox", format_move!("0 0 {} {}", width, height)),
+            ("style", "background-color: white;")
+        ));
+
+        let title = build::elem("text")
+            .with(attrs!(
+                ("class", "title"),
+                ("x", width / 2.0),
+                ("y", rd.gutter.top / 2.0)
+            ))
+            .append(build::elem(format_move!("{}", &rd.title)));
+
+        let mut output = String::new();
+        let all = svg.append(style).append(title);
+
+        hypermelon::render(all, &mut output)?;
+
+        Ok(output)
     }
 }
 
